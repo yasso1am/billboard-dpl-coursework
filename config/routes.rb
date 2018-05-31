a17e87b171134372b1858d5023924d935285e77d
Andrew Yasso <andrew.yasso@gmail.com>
2018-05-31 16:03:05 -0600

    routing changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   root 'staticpages#home'
 
   resources :boards do
-    resources :artists
     resources :songs
   end
   
-  # scope 'artists/:artist_id', as: 'artist' do
-  #   resources :songs, only: [:new, :create]
-  # end
+  scope 'artists/:artist_id', as: 'artist' do
+    resources :songs, only: [:new, :create]
+  end
 
 end
