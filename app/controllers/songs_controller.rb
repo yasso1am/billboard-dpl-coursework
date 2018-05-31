20d9097839c045f097b7c7bb68602557d0fd29bc
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-05-31 15:48:06 -0600

    update songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,38 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+  before_action :set_song, only [:show, :edit, :update, :destroy]
+  
+  def index
+    @songs = @artist.songs
+
   def show
   end
 
   def new
+    @song = @artist.songs.new
+    render partial: "form"
   end
 
   def edit
+    render partial: "form"
   end
 
-  def index
+  def create
+    @song = @artist.songs.new(song_params)
+    
+    if @song.save
+    redirect_to [@artist, @song]
+
+    else 
+      render :new
+    end
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to [@artist, @song]
+    else
+      render :edit
+    end
   end
 end
