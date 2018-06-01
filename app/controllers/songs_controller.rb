35eafc94709c3947973d84db22f447753617cede
Andrew Yasso <andrew.yasso@gmail.com>
2018-06-01 16:09:47 -0600

    giving up

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,26 @@
 class SongsController < ApplicationController
-  before_action :set_artist, except: [:all, :addtoboard]
+  before_action :set_artist, except: [:all, :addtoboard, :boardupdate]
+    # before_action :set_artist, only: [:index, :show, :new, :edit, :create, :update, :destroy]
+  before_action :set_board, only: [:addtoboard, :boardupdate]
   before_action :set_song, only: [:show, :edit, :update, :destroy]
+
+  def addtoboard
+    @songs = Song.all
+  end
+
+  def boardupdate
+    binding.pry
+    if @board.update(song_params)
+      redirect_to [@board]
+    else
+      render :addtoboard
+    end
+  end
 
   def all
     @songs = Song.all
-    @artists = Artist.all
   end
+
 
 
   def index
@@ -57,7 +72,12 @@ private
     @song = Song.find(params[:id])
   end
 
+  def set_board
+    @board = Board.find(params[:board_id])
+  end
+
   def song_params
-    params.require(:song).permit(:name, :rating)
+    binding.pry
+    params.require(:song).permit(:name, :rating, :board_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,18 @@ Rails.application.routes.draw do
   root 'staticpages#home'
 
   resources :boards do
-    resources :songs
+    get 'songs/addtoboard', to: 'songs#addtoboard'
+    post 'songs/addtoboard/:song_id', to: 'songs#boardupdate'
   end
-
+  
+  
   resources :artists do
     resources :songs
   end
   
   get 'songs/all'
 
-  get 'songs/addtoboard'
+  
 
   scope 'artists/:artist_id', as: 'artist' do
     resources :songs, only: [:new, :create]
